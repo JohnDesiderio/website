9d3c28891752bb351dfeecb2ea011c56590c8e65
John Desiderio <johnfrancisdesiderio@gmail.com>
2023-11-27 08:49:00 -0500

    Added about section

--- src/portfolio/pages/About.tsx
+++ src/portfolio/pages/About.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import Header from '../components/header/header';
+import '../styles.css';
 
 export const About:React.FC<{}> = () => {
     
     return (
-        <Box>
+        <Box display='flex' flexDirection='column' className='bg-red-50 portfolio-page'>
             <Header value={3} />
-            About, About
+            <Box
+                className='page-header'
+            >
+                About
+            </Box>
+            <Box className='bio-text'>
+                My name is John Desiderio, and I am an aspiring software engineer 
+                open to all opportunities. I am originally from Atlanta, 
+                GA, but I moved to Statesboro, GA when I was nine. Growing 
+                up, I played soccer and tried my hardest never to miss 
+                an opportunity to kick a ball. Likewise, I enjoy watching 
+                soccer, but my sports interests are not limited to soccer. 
+                I also love to watch college football and Formula 1. After 
+                graduating high school, I attended the University of Georgia 
+                and spent 4.5 years in Athens. I graduated with a Bachelor 
+                of Science in computer science and a Bachelor of Arts in 
+                English. Now, I am looking for a position that would allow 
+                me to enter the workforce and work on the new, cool, and 
+                exciting technology. I maintain my web apps in my free time, 
+                but if I am away from the screen, I prefer to read or spend 
+                time with friends. If you want to connect with me on 
+                Goodreads, you should click the link and befriend me!
+            </Box>
         </Box>
     )
 }
